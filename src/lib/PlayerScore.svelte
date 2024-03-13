0829e6e7823a6b8ecbc736c89e9da610ebb64873
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-13 12:53:55 -0600

    try binding to child to reset component values, add styles

--- src/lib/PlayerScore.svelte
+++ src/lib/PlayerScore.svelte
@@ -6,18 +6,38 @@
 
     function incrementPlayerScore() {
         playerScore += 1;
-    }
+    };
 
     function decreasePlayerScore() {
         if(playerScore > 0) {
             playerScore -= 1;
         }
-    }
+    };
+    
+    export function resetScore() {
+        playerScore = 0;
+    };
 </script>
 
 <div>
     <h2>{ playerName }</h2>
     <p>{ playerScore }</p>
     <button on:click={incrementPlayerScore}>Up</button>
-    <button on:click={decreasePlayerScore}>down</button>
+    <button on:click={decreasePlayerScore} disabled={playerScore === 0}>Down</button>
 </div>
+
+<style>
+    div {
+        padding: 1rem;
+        margin: 1rem;
+    }
+    h2 {
+        margin: 0;
+    }
+    p {
+        margin: 0;
+    }
+    button {
+        margin: 5px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,37 @@
 <script>
-  import PlayerScore from "$lib/PlayerScore.svelte";
+    import PlayerScore from "$lib/PlayerScore.svelte";
 
+    /** @type {PlayerScore} **/
+    let player1Score;
+
+    /** @type {PlayerScore} **/
+    let player2Score;
+
+    function resetScore() {
+        player1Score.resetScore();
+        player2Score.resetScore();
+    }
 </script>
 
 <main>
     <h1>Home page</h1>
-    <PlayerScore playerName="Player 1"></PlayerScore>
-    <PlayerScore playerName="Player 2"></PlayerScore>
+
+    <h2>Player Score Counter:</h2>
+    <PlayerScore bind:this={player1Score} playerName="Player 1"></PlayerScore>
+    <PlayerScore bind:this={player2Score} playerName="Player 2"></PlayerScore>
+
+    <button on:click={resetScore}>Reset</button>
 </main>
 
-<style></style>
+<style>
+    main {
+        padding: 1rem;
+        margin: 1rem;
+    }
+    h1 {
+        margin: 0;
+    }
+    h2 {
+        margin: 2rem;
+    }
+</style>

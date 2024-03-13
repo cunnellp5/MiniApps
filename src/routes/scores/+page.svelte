523d4ea17f1cdb14a7da88e55d0668ca3a0beb8a
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-13 13:15:47 -0600

    move score keeper to new route

---
+++ src/routes/scores/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import PlayerScore from "$lib/PlayerScore.svelte";
+
+    /** @type {Number} **/
+    let player1Score;
+
+    /** @type {Number} **/
+    let player2Score;
+
+    function resetScore() {
+        player1Score = 0;
+        player2Score = 0;
+    }
+</script>
+
+<div>
+    <h1>Player Score Counter:</h1>
+    <PlayerScore bind:playerScore={player1Score} playerName="Player 1"></PlayerScore>
+    <PlayerScore bind:playerScore={player2Score} playerName="Player 2"></PlayerScore>
+    <button on:click={resetScore}>Reset</button>
+</div>

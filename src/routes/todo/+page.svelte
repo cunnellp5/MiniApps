938b0d8b3efa6c35ea36d294a622d082d3f92f44
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-13 14:42:57 -0600

    watching for changes and using localstorage

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -3,6 +3,13 @@
 
     /** @type {{ title: string; done: boolean; }[]} **/
     let todos = [];
+    if (typeof window !== 'undefined') {
+        todos = localStorage.getItem('todos') 
+            // @ts-ignore
+            ? JSON.parse(localStorage.getItem('todos'))
+            : [];
+    }
+
 
     /** @param {string} todo **/
     function addTodo(todo) {
@@ -21,6 +28,11 @@
         todos.splice(index, 1);
         todos = todos;
     }
+
+    
+    $: if (typeof window !== 'undefined') {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }
 </script>
 
 <main>
@@ -33,21 +45,21 @@
             <!-- svelte-ignore a11y-no-static-element-interactions -->
             {#each todos as { title, done }, i}
                 <li class="todo" class:done={done}>
+                    <!-- <div class="deleteWrapper"> -->
                     {#if done}
                         <span>✅</span>
                     {/if}
-                    <div class="deleteWrapper">
-                        <!-- svelte-ignore a11y-click-events-have-key-events -->
-                        <span on:click={() => done = !done}>
-                            {title}
-                        </span>
-                        <!-- svelte-ignore a11y-click-events-have-key-events -->
-                        <span 
-                            class="delete" 
-                            on:click={() => removeTodos(i)}>
-                            🗑️
-                        </span>
-                    </div>
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <span on:click={() => done = !done}>
+                        {title}
+                    </span>
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <span 
+                        class="delete" 
+                        on:click={() => removeTodos(i)}>
+                        🗑️
+                    </span>
+                    <!-- </div> -->
                 </li>
             {/each}
         </ul>
@@ -55,11 +67,11 @@
 </main>
 
 <style>
-    .deleteWrapper {
+    /* .deleteWrapper {
         display: flex;
         justify-content: space-between;
         align-items: center;
-    }
+    } */
     .todosWrapper {
         margin: 0 2rem;
         width: 360px;

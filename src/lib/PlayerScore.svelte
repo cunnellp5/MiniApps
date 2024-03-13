82614e785c464367e8dfeff24a6bd861c9bdbe4f
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-13 12:38:33 -0600

    learning componenets and props by building a score keeper

---
+++ src/lib/PlayerScore.svelte
@@ -0,0 +1,23 @@
+<script>
+    /** @type {string} **/
+     export let playerName;
+
+    let playerScore = 0;
+
+    function incrementPlayerScore() {
+        playerScore += 1;
+    }
+
+    function decreasePlayerScore() {
+        if(playerScore > 0) {
+            playerScore -= 1;
+        }
+    }
+</script>
+
+<div>
+    <h2>{ playerName }</h2>
+    <p>{ playerScore }</p>
+    <button on:click={incrementPlayerScore}>Up</button>
+    <button on:click={decreasePlayerScore}>down</button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
-<h1>Test blog</h1>
+<script>
+  import PlayerScore from "$lib/PlayerScore.svelte";
+
+</script>
+
+<main>
+    <h1>Home page</h1>
+    <PlayerScore playerName="Player 1"></PlayerScore>
+    <PlayerScore playerName="Player 2"></PlayerScore>
+</main>
+
+<style></style>

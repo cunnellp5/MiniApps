f2ec1d620b9c7d761a3e130ed99b2d1b89b734cc
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-13 13:15:38 -0600

    add layout styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,61 @@
 <nav>
-  <a href="/">home</a>
-  <a href="/posts">posts</a>
+  <a class="hover-line" href="/">Home</a>
+  <a class="hover-line" href="/scores">Scores</a>
+  <a class="hover-line" href="/posts">Posts</a>
 </nav>
 
-<slot />
+<main>
+  <slot />
+</main>
+
+
+<style>
+  main {
+      padding: 1rem;
+      margin: 1rem;
+  }
+
+
+  nav {
+    position: relative;
+    /* margin: 270px auto 0; */
+    /* width: 520px; */
+    height: 50px;
+    background-color: #6e6e6e;
+    font-size: 0;
+    box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.1);
+  }
+
+  nav a{
+    font-size: 15px;
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+    position: relative;
+    z-index: 1;
+    display: inline-block;
+    text-align: center;
+    padding: 0 20px;
+    line-height: 50px;
+    
+  }
+
+  nav a:hover{
+    color: #449f97;
+  }
+
+  .hover-line::after{
+    content: "";
+    display: block;
+    width: 0;
+    height: 2px;
+    background-color: #449f97;
+    transition: all .6s ease 0s;
+    transition: width .3;  
+  }
+
+  .hover-line:hover::after{
+    width: 100%;
+    transition: width .34s;
+  }
+</style>

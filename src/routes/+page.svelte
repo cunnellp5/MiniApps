3b95a7be1f7056a0951065cbf5066e0782349144
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-13 16:53:24 -0600

    adding threejs animations to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,15 @@
-<script type="x-shader/x-fragment" id="fragment-shader">
-    // #version 100
-    void main() {
-      gl_FragColor = vec4(0.18, 0.54, 0.34, 1.0);
-    }
-  </script>
+<script>
+    import Cube from '../lib/Cube.svelte';
+</script>
 
-<h1>Home page</h1>
-<h3>Description:</h3>
-<p>
-    This is a playground to learn svelte.
-</p>
-<img src="" alt="">
+<main>
+    <h1>Home page</h1>
 
-<style>
+    <h3>Description:</h3>
+    <p>
+        This is a playground to learn svelte. Also here's a rotating cube made with three.js
+    </p>
+    <Cube></Cube>
+</main>
 
-</style>
+<style></style>

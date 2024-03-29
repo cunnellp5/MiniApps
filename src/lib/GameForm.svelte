e93677184563997132e2ac61cc73aefd268255e7
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-29 14:37:11 -0400

    refactor scoring to show whole games with n number of players each keeping track of their own scores

---
+++ src/lib/GameForm.svelte
@@ -0,0 +1,89 @@
+<script lang='ts'>
+  export let addGame;
+
+  type Player = { name: string, score: number };
+
+  let gameName: string = '';
+  let players: Player[] = [];
+  let playerCount: number = 0;
+
+  function formSubmit() {
+    console.log("game created");
+    addGame({ gameName, players })
+
+    gameName = '';
+    playerCount = 0;
+    players = [];
+  }
+
+  $: {
+    players = Array(playerCount)
+      .fill(null)
+      .map((_, i) => ({ name: `Player ${i + 1}`, score: 0 }));
+  }
+</script>
+
+<form on:submit|preventDefault={formSubmit} action="" method="post">
+    <label>
+        <input 
+          bind:value={gameName}
+          type="text" 
+          placeholder="Enter a game name...">
+    </label>
+    <label>
+        <input 
+          bind:value={playerCount} 
+          type="number"
+          min="0"
+          placeholder="how many players?"
+        >
+    </label>
+    {#if playerCount > 0 && gameName.length > 0}
+      {#each players as player, i}
+        <label>
+            <input
+              bind:value={player.name}
+              type="text" 
+              placeholder="Player name"
+            >
+        </label>
+      {/each}
+      <button>add game</button>
+    {/if}
+</form>
+
+<style>
+    form {
+        z-index: 1;
+        background: #FFFFFF;
+        max-width: 360px;
+    }
+    form input {
+        font-family: "Roboto", sans-serif;
+        outline: 0;
+        background: #f2f2f2;
+        width: 100%;
+        border: 0;
+        margin: 0 0 15px;
+        padding: 15px;
+        box-sizing: border-box;
+        font-size: 14px;
+    }
+    form button {
+        font-family: "Roboto", sans-serif;
+        text-transform: uppercase;
+        outline: 0;
+        background: #4CAF50;
+        width: 100%;
+        border: 0;
+        padding: 10px;
+        color: #FFFFFF;
+        font-size: 14px;
+        -webkit-transition: all 0.3 ease;
+        transition: all 0.3 ease;
+        cursor: pointer;
+    }
+    form button:hover,form button:active,form button:focus {
+        background: #43A047;
+    }
+</style>
--- src/routes/scores/+page.svelte
+++ src/routes/scores/+page.svelte
@@ -1,30 +1,73 @@
-<script>
+<script lang='ts'>
     import PlayerScore from "$lib/PlayerScore.svelte";
+    import GameForm from "$lib/GameForm.svelte";
+  import { GammaCorrectionShader } from "three/examples/jsm/Addons.js";
 
-    /** @type {Number} **/
-    let player1Score;
+    /** @type {boolean} **/
+    let showResetButton = false;
 
-    /** @type {Number} **/
-    let player2Score;
+    /** @type {Players} **/
+    type Players = { name: string, score: number }
 
-    function resetScore() {
-        player1Score = 0;
-        player2Score = 0;
+    /** @type {Game} **/
+    type Game = {
+        players: Players[],
+        gameName: string
+    }
+
+    let games:Game[] = [];
+
+    // let game: Game = {
+    //     players: [
+    //         { name: "Player 1", score: 0 },
+    //         { name: "Player 2", score: 0 }
+    //     ],
+    //     gameName: "Tic Tac Toe"
+    // }
+
+    function addGame (game) { 
+        console.log(game, "game created");
+        games.push(game);
+        games = games
+    }
+
+    function resetScore(game) {
+       game.players = game.players.map(player => ({ ...player, score: 0 }));
+    }
+
+    $: {
+        console.log(games, 'testing')
+        // games.forEach((game) => {
+        //     showResetButton = game.players.some(player => player.score > 0)
+        // })
     }
 </script>
 
-<h1>Player Score Counter:</h1>
-<div class=scoreWrapper>
-    <PlayerScore bind:playerScore={player1Score} playerName="Player 1"></PlayerScore>
-    <PlayerScore bind:playerScore={player2Score} playerName="Player 2"></PlayerScore>
-    {#if player1Score > 0 || player2Score > 0}
-        <button on:click={resetScore}>RESET</button>
-    {/if}
-</div>
+<h1>Score:</h1>
+
+<GameForm addGame={addGame}/>
+
+{#each games as game}
+    <!-- <Game game={game} showResetButton={showResetButton} resetScore={resetScore}></Game> -->
+    <div class=gameWrapper>
+        Game {game.gameName}
+    
+        {#each game.players as player}
+            <PlayerScore bind:playerScore={player.score} playerName={player.name}></PlayerScore>        
+        {/each}
+<!--     
+        {#if showResetButton}
+            <button on:click={() => resetScore(game)}>RESET</button>
+        {/if} -->
+    </div>
+{/each}
 
 <style>
-    .scoreWrapper {
+    .gameWrapper {
         margin: 0 2rem;
+        border: 1px solid #f4f4f4;
+        box-shadow: 1px 1px 5px 0 rgba(0, 0, 0, 0.08);
+        padding: 16px;
     }
 
     button {

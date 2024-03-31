35b1d78bb88dae1e536931a7b3afae77b5a61912
cunnellp5 <philip.cunnell@colorado.edu>
2024-03-30 21:40:13 -0400

    debugging

---
+++ src/routes/r/[slug]/+page.ts
@@ -0,0 +1,18 @@
+import { error } from "@sveltejs/kit";
+
+/** @type {import('./$types').PageLoad} **/
+export async function load({ fetch, params }: { fetch: any; params: string }) {
+  const url = `https://api.reddit.com/r/${params.slug}.json`;
+
+  try {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error("Response not OK");
+    }
+
+    return { posts: (await res.json()).data.children };
+  } catch (error) {
+    throw error;
+  }
+}

9db2ec228fb44858fad684101acca788f88202fe
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-03 09:42:30 -0600

    implement server only load fn

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,7 @@
 import type { LayoutServerLoad } from "./$types";
 
+export const prerender = true;
+
 export const load: LayoutServerLoad = async (event) => {
   return {
     session: await event.locals.auth(),

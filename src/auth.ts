452b124d8d3a41d696e998b9c5dbdbc1838e74fd
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-03 09:42:45 -0600

    just use github for now

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,8 @@
 import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private";
 import { SvelteKitAuth } from "@auth/sveltekit";
-import Facebook from "@auth/sveltekit/providers/facebook";
 import GitHub from "@auth/sveltekit/providers/github";
-import Google from "@auth/sveltekit/providers/google";
+// import Facebook from "@auth/sveltekit/providers/facebook";
+// import Google from "@auth/sveltekit/providers/google";
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
   providers: [

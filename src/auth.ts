d9de92bf84085a7299623843f87c9009025053e2
cunnellp5 <philip.cunnell@colorado.edu>
2024-04-03 14:37:00 -0600

    rm and comment

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,10 @@
 import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private";
 import { SvelteKitAuth } from "@auth/sveltekit";
 import GitHub from "@auth/sveltekit/providers/github";
-// import Facebook from "@auth/sveltekit/providers/facebook";
-// import Google from "@auth/sveltekit/providers/google";
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
   providers: [
     GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
-    // Google, // figure this out
-    // Facebook, // figure this out
+    // https://authjs.dev/guides/providers/email
   ],
 });
